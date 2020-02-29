7ffda1f76ffe2ed7117ad2b63b78b4f028025c84
Mac Goodwin <mac.j.goodwin@gmail.com>
2020-02-29 13:13:59 +0000

    Skeleton class for NPC

---
+++ NPCs/npc.py
@@ -0,0 +1,27 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
+import utils
+
+class NPC:
+    """parent class for various NPC classes"""
+
+    #attributes
+    health = 0
+    speed = 0
+
+    #combat attributes
+    melee_attack_bonus = 0
+    melee_damage_die = 0
+    melee_damage_bonus = 0
+
+    has_ranged_attack = False
+    ranged_attack_bonus = 0
+    ranged_damage_die = 0
+    ranged_damage_bonus = 0
+
+    def interact(player):
+        """method for interacting with player
+        defines extra dialogue options and paths to combat/social/skill encounters"""
